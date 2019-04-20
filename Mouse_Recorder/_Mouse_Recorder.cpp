4ada3099f1a08478f9fc63a9f4bfc0e276a29597
Damian Andrysiak <andrydamian@gmail.com>
2019-04-20 16:31:41 +0200

    working clock

--- Mouse_Recorder/_Mouse_Recorder.cpp
+++ Mouse_Recorder/_Mouse_Recorder.cpp
@@ -4,21 +4,59 @@ using namespace std;
 using namespace std::chrono;
 using namespace std::chrono_literals;
 
+void _Mouse_Recorder::SetCursorPosition(const short _X_AXIS, const short _Y_AXIS)
+{
+	CORD.X = _X_AXIS;
+	CORD.Y = _Y_AXIS;
+	SetConsoleCursorPosition(H_OUT, CORD);
+}
+
+void _Mouse_Recorder::Clock_Format() const
+{
+	if (second < 10 && minute < 10)
+	{
+		cout << "Elapsed time: "  << '0' << minute << ':' << '0' << second;
+	}
+	else if(second >= 10 && minute < 10)
+	{
+		cout << "Elapsed time: " << '0' << minute << ':' << second;
+	}
+	else if (second < 10 && minute >= 10)
+	{
+		cout << "Elapsed time: " << minute << ':' << '0' << second;
+	}
+	else
+	{
+		cout << "Elapsed time: " << minute << ':' << second;
+	}
+}
+
+LONG _Mouse_Recorder::get_cursor_position_x() const
+{
+	return Cursor_Pos.x;
+}
+
+LONG _Mouse_Recorder::get_cursor_position_y() const
+{
+	return Cursor_Pos.y;
+}
+
 _Mouse_Recorder::_Mouse_Recorder() :
-	m_start(),
-	m_stop(),
 	minute(0),
 	second(0)
 {
 	/*t = time(0);
 	now = localtime(&t);*/
+	H_OUT = GetStdHandle(STD_OUTPUT_HANDLE);
+	H_IN = GetStdHandle(STD_INPUT_HANDLE);
 }
 
 void _Mouse_Recorder::Record()
 {
 	//seconds = now->tm_sec;
 	//minutes = now->tm_min;
-	m_start = high_resolution_clock::now();
+//	m_start = high_resolution_clock::now();
+	m_start = clock::now();
 	bool record = true;
 	while (record == true)
 	{
@@ -32,28 +70,32 @@ void _Mouse_Recorder::Record()
 
 void _Mouse_Recorder::Clock()
 {
-	m_stop = high_resolution_clock::now();
 	second = duration_cast<seconds>(clock::now() - m_start).count();
-	if (second % 5 == 0 && second > 0)
+	
+	if (second % 60 == 0 && second > 0)
 	{
 		++minute;
-		/*m_start.*/
+		m_start = clock::now();
 		second = 0;
 	}
-	cout << "Elapsed time: " << minute << ':' << second << '\n';
-	//
-	//if ((m_stop - m_start).count() == 5)
-	//{
-	//	++minutes;
-	//}
-	////cout << "It took me equal: " << duration_cast<seconds>(m_stop - m_start).count() << " seconds" << endl;
-	//cout << "Elapsed time: " << minutes << ':' << duration_cast<seconds>(m_stop - m_start).count() << '\n';
-	////system("cls");
+	SetCursorPosition(get_cursor_position_x() + 1, get_cursor_position_y() + 1);
+	Clock_Format();
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	//std::chrono::steady_clock::time_point Restart();
+	Erase_Row(get_cursor_position_x() + 1, get_cursor_position_y() + 1);
+}
+
+void _Mouse_Recorder::Erase_Row(const short CORD_X, const short CORD_Y)
+{
+	SetCursorPosition(CORD_X, CORD_Y);
+	for (size_t i = 0; i < 30; ++i)
+	{
+		std::cout << ' ';
+	}
+	SetCursorPosition(CORD_X, CORD_Y);
 }
 
 _Mouse_Recorder::~_Mouse_Recorder()
 {
-
+	second = 0;
+	minute = 0;
 }
--- Mouse_Recorder/_Mouse_Recorder.hpp
+++ Mouse_Recorder/_Mouse_Recorder.hpp
@@ -25,9 +25,26 @@ private:
 	/*time_t t;
 	struct tm * now;*/
 	__int16 minute, second;
+	//using clock = std::chrono::steady_clock;
+	//std::chrono::high_resolution_clock::time_point m_start;			//"zmienna" ktora bedzie trzymac czas startowy		(czesc struktury steady_clock, przestrzeni nazw chrono)
+	//std::chrono::high_resolution_clock::time_point m_stop;			//"zmienna" ktora bedzie trzymac czas koncowy		(czesc struktury steady_clock, przestrzeni nazw chrono)
 	using clock = std::chrono::steady_clock;
-	std::chrono::high_resolution_clock::time_point m_start;			//"zmienna" ktora bedzie trzymac czas startowy		(czesc struktury steady_clock, przestrzeni nazw chrono)
-	std::chrono::high_resolution_clock::time_point m_stop;			//"zmienna" ktora bedzie trzymac czas koncowy		(czesc struktury steady_clock, przestrzeni nazw chrono)
+	clock::time_point m_start;
+	COORD CORD;						//Obiekt struktury COORD, w ktorym znajduja sie koordynaty X i Y do ustawienia kursora
+	HANDLE H_OUT, H_IN;				//Zmienna przechowujace uchywyt do outputu oraz inputu okna
+	POINT Cursor_Pos;						//current cursor position
+	//////////////////////////////////////////////////////////////////////////////
+	/*
+		FUNKCJE PRIVATE
+	*/
+	void SetCursorPosition(const short _X_AXIS, const short _Y_AXIS);	//Metoda ustawiajaca pozycje kursora
+	void Clock_Format() const;
+	//////////////////////////////////////////////////////////////////////////////
+	/*
+		GETTERY PRIVATE
+	*/
+	LONG get_cursor_position_x() const;
+	LONG get_cursor_position_y() const;
 	//////////////////////////////////////////////////////////////////////////////
 public:
 	//////////////////////////////////////////////////////////////////////////////
@@ -41,6 +58,7 @@ public:
 	*/
 	void Record();
 	void Clock();
+	void Erase_Row(const short CORD_X, const short CORD_Y);
 	//////////////////////////////////////////////////////////////////////////////
 	/*
 		SETTERY PUBLIC
